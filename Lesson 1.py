84139d1ea3c1fb845497701bf39e041247d00413
ry <yeryanyin@gmail.com>
2026-03-23 09:25:55 -0400

    so bad
    What Ernst have

---
+++ Lesson 1.py
@@ -0,0 +1,14 @@
+class Hero:
+    def __init__(self, name, money, inventory):
+        self.name = name
+        self.money = money
+        self.inventory = inventory
+
+    def buy(self, item):
+        self.inventory.append(item)
+        print(self.inventory)
+
+Ernst = Hero("Ernst", 1000000, ["Flag of Libya"])
+
+Ernst.buy({"title": "Toyota", "speed": 90, "atk": 380})
+print(Ernst.__dict__)
