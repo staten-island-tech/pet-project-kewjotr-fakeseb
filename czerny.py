ec1559fb26154fddb0ca6a6d3ca4d5a1566ef32d
ry <yeryanyin@gmail.com>
2026-04-15 09:19:12 -0400

    Update czerny.py

--- czerny.py
+++ czerny.py
@@ -1,3 +1,5 @@
+import random
+
 class Hero:
     def __init__(self, name, health, inventory):
         self.name = name
@@ -8,5 +10,6 @@ class shopping:
     def market(self, marketitems, balance):
         self.marketitems = marketitems()
         self.balance = balance
+    def buy
     
 HA_Sosa = Hero("HA Sosa", 100, "Silver Pot")
