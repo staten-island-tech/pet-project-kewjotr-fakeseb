24eada9e52b254fb61c684b5fb126c343e4b3c7c
ry <yeryanyin@gmail.com>
2026-04-20 08:48:20 -0400

    Update czerny.py

--- czerny.py
+++ czerny.py
@@ -1,18 +1,42 @@
 import random
-lottery = random.randint(0,5)
+[Tape = {
+    "Name": "Tape",
+    "Price": 2.00,
+}
+
+
+
 
 class Hero:
-    def __init__(self, name, health, inventory):
+    def __init__(self, name, health, inventory, balance):
         self.name = name
         self.health = health
         self.inventory = inventory
-
-class shopping:    
-    def market(self, marketitems, balance):
-        self.marketitems = marketitems()
         self.balance = balance
+class Market:
+    def marketitems(self):
+        print("Tape", "Chess Board", "Heinz Ketchup", "Ice Cream", "Woodwork", "Sand", "Flag of Libya", "Plank", "Weedspray", "Turk", "Crusader Kings 3")
+
+    def buy(self):
+
 
     def gamble(self, lottery):
+        lottery = random.randint(0,5)
         if lottery == 0:
-    
-James = Hero("James", 100, "Silver Pot")
+
+    def choice(self, thetower):
+        if thetower == "checkmarket" or thetower == "market":
+            Market.marketitems()
+        if thetower == "gamble":
+
+class payed_objected:
+    def __init__(self,price):
+        self.price = price
+
+
+James = Hero("James", 100, "Silver Pot", 1000)
+Tape = Market()
+
+while True:
+    thetower = input()
+    Market.choice(thetower)
