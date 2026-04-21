c4ef978ba143d6c88b46679f54acfcbefc6efe1e
ry <yeryanyin@gmail.com>
2026-04-21 09:02:58 -0400

    trier

--- czerny.py
+++ czerny.py
@@ -1,35 +1,28 @@
-class Objects:
-    def __init__(self, name, price):
-        self.name = name
-        self.price = price
 class Hero:
-    def __init__(self, name, health, balance):
+    def __init__(self, name, inventory, health, balance):
         self.name = name
         self.health = health
-        self.inventory = []
+        self.inventoy = inventory
         self.balance = balance
 
 class Market:
     def __init__(self,items):
         self.items = items
-    def marketitems(self):
+    
+    def check(self):
         print(Market.items)
+
     def buy(self):
-        purchase = input()
-        Hero.balance - Objects.price
-    def choice(self, thetower):
-        if thetower == "checkmarket" or thetower == "market":
-            Market.marketitems()
-        if thetower == "buy" or thetower == "purchase":
-            Market.buy()
-
-class payed_objected:
-    def __init__(self,price):
-        self.price = price
-
+        buying = input("Select")
+        if buying in Market.items:
+            
 
 James = Hero("James", 100, 1000)
+Stock = Market("")
 
 while True:
-    thetower = input()
-    Market.choice(thetower)
+    action = input("Do an action")
+    if (action == "checkmarket") or (action == "check"):
+        Stock.check()
+    if (action == "buy") or (action == "purchase"):
+        Stock.buy()
